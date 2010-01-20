8edf7ce9bd3bf2754d8113534acf7a88a66284a9
rich2000 <rich2000@e5216afe-46cd-4a6c-a0e4-fb0756412cb9>
2010-01-20 16:18:13 +0000

    Add exchange_addressbook to showInAddressBook schema mapping

--- adLDAP.php
+++ adLDAP.php
@@ -2030,6 +2030,7 @@ class adLDAP {
         if ($attributes["exchange_usedefaults"]){ $mod["mDBUseDefaults"][0]=$attributes["exchange_usedefaults"]; }
         if ($attributes["exchange_policyexclude"]){ $mod["msExchPoliciesExcluded"][0]=$attributes["exchange_policyexclude"]; }
         if ($attributes["exchange_policyinclude"]){ $mod["msExchPoliciesIncluded"][0]=$attributes["exchange_policyinclude"]; }       
+        if ($attributes["exchange_addressbook"]){ $mod["showInAddressBook"][0]=$attributes["exchange_addressbook"]; }       
         
         // This schema is designed for contacts
         if ($attributes["exchange_hidefromlists"]){ $mod["msExchHideFromAddressLists"][0]=$attributes["exchange_hidefromlists"]; }
